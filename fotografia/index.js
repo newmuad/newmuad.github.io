5ba6b3a6a6c97de6f2d127781d658a4fd952e01c
newmuad <118389813+newmuad@users.noreply.github.com>
2024-02-18 14:33:38 +0100

    Add files via upload

---
+++ fotografia/index.js
@@ -0,0 +1,85 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('header', {
+        duration: 3000,
+        origin: 'top',
+        distance: '10px',
+    });
+
+    sr.reveal('.video', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '80px',
+    });
+
+    sr.reveal('.img-container', {
+        duration: 4000,
+        origin: 'left',
+        distance: '100px'
+    });
+    sr.reveal('.texto', {
+        duration: 4000,
+        origin: 'right',
+        distance: '100px'
+    });
+
+    sr.reveal('.perfil', {
+        duration: 4000,
+        origin: 'left',
+        distance: '200px',
+    });
+    sr.reveal('.foto-perfil', {
+        duration: 4000,
+        origin: 'right',
+        distance: '100px',
+    });
+
+    sr.reveal('.#contenido h2', {
+        duration: 4000,
+        origin: 'top',
+        distance: '20px'
+    });
+    sr.reveal('.columna1, .img-li', {
+        duration: 4000,
+        origin: 'left',
+        distance: '100px'
+    });
+    sr.reveal('.columna2, .img-ld', {
+        duration: 4000,
+        origin: 'right',
+        distance: '100px'
+    });
+    sr.reveal('#contenido a, .extra', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '100px'
+    });
+
+    sr.reveal('.copy', {
+        duration: 4000,
+        origin: 'top',
+        distance: '10px'
+    });
+    sr.reveal('#opiniones .titulo', {
+        duration: 4000,
+        origin: 'top',
+        distance: '10px'
+    });
+
+    sr.reveal('.opinion', {
+        duration: 2000,
+        origin: 'left',
+        distance: '200px'
+    });
+
+    sr.reveal('#caracteristicas h1, .carta', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '30px'
+    });
+
+    sr.reveal('.copy-bonus, .copy-oferta', {
+        duration: 4000,
+        origin: 'left',
+        distance: '100px'
+    });
